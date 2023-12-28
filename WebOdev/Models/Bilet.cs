b40fa5bc926079961062a59d2955a95a295eb722
Mehmet Akif Balcı <akif.balci@ogr.sakarya.edu.tr>
2023-12-28 23:06:46 +0300

    Css & BiletKontrol

---
+++ WebOdev/Models/Bilet.cs
@@ -0,0 +1,14 @@
+﻿namespace WebOdev.Models
+{
+    public class Bilet
+    {
+        public int Id { get; set; }
+        public string KoltukNumarasi { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string GidisNoktasi { get; set; }
+        public string VarisNoktasi { get; set; }
+        public DateTime KalkisTarihi { get; set; }
+    }
+
+}
