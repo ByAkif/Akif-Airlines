0df759c5f54a69717067cef6089e43761c3edf96
Mehmet Akif Balcı <akif.balci@ogr.sakarya.edu.tr>
2023-12-30 00:02:01 +0300

    Uçuş kontrol Düzenleme

---
+++ WebOdev/Controllers/UcusController.cs
@@ -0,0 +1,85 @@
+﻿using H12Auth2C.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+
+namespace H12Auth2C.Controllers
+{
+   
+    public class UcusKontrolController : Controller
+    {
+ 
+
+public class UcusController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UcusController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Uçuş listesini gösteren sayfa
+        public IActionResult Index()
+        {
+            var ucuslar = _context.Ucuslar.ToList();
+            return View(ucuslar);
+        }
+
+        // Uçuş detaylarını gösteren sayfa
+        public IActionResult Detay(int id)
+        {
+            var ucus = _context.Ucuslar.Find(id);
+
+            if (ucus == null)
+            {
+                return NotFound();
+            }
+
+            return View(ucus);
+        }
+
+        // Uçuş rezervasyon formu
+        public IActionResult Rezervasyon(int id)
+        {
+            var ucus = _context.Ucuslar.Find(id);
+
+            if (ucus == null)
+            {
+                return NotFound();
+            }
+
+            return View(ucus);
+        }
+
+        // Rezervasyon işlemleri
+        [HttpPost]
+        public IActionResult Rezervasyon(int id, string koltukNumarasi, string adSoyad)
+        {
+            var ucus = _context.Ucuslar.Find(id);
+
+            if (ucus == null)
+            {
+                return NotFound();
+            }
+
+            // Rezervasyon işlemleri burada yapılır (veritabanına kaydetme vb.)
+
+            return RedirectToAction("RezervasyonOnay", new { id = id, koltukNumarasi = koltukNumarasi, adSoyad = adSoyad });
+        }
+
+        // Rezervasyon onay sayfası
+        public IActionResult RezervasyonOnay(int id, string koltukNumarasi, string adSoyad)
+        {
+            // Rezervasyon bilgilerini görüntüleme
+
+            return View();
+        }
+    }
+
+}
+
+}
---
+++ WebOdev/Models/Ucus.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net.Sockets;
+using System.Numerics;
+
+namespace H12Auth2C.Models
+{
+    public class UcusKontrol
+    {
+        public int UcusId { get; set; }
+        public string GidisNoktasi { get; set; }
+        public string VarisNoktasi { get; set; }
+        public DateTime KalkisTarihi { get; set; }
+        public DateTime VarisTarihi { get; set; }
+        public bool KontrolDurumu { get; set; }
+    }
+
+}
