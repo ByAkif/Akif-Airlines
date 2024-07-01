d37ad25ba81e6d0e348360e8d9cf7e95754a4e51
ByAkif <makifbalci1@gmail.com>
2024-07-01 15:01:42 +0300

    hmm

--- WebOdev/Services/LanguageServices.cs
+++ WebOdev/Services/LanguageServices.cs
@@ -1,23 +1,25 @@
-﻿using Microsoft.Extensions.Localization;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
 using System.Reflection;
 
-namespace WebProgramlamaOdev.Services
+namespace WebProgramlamaProje.Services
 {
     public class SharedResource
     {
 
     }
-    public class LanguageService
+    public class LanguageServices
     {
         private readonly IStringLocalizer _localizer;
-
-        public LanguageService(IStringLocalizerFactory factory)
+        public LanguageServices(IStringLocalizerFactory factory) 
         {
             var type = typeof(SharedResource);
             var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
             _localizer = factory.Create(nameof(SharedResource), assemblyName.Name);
+
         }
-        public LocalizedString Getkey(string key)
+
+        public LocalizedString GetKey(string key)
         {
             return _localizer[key];
         }
